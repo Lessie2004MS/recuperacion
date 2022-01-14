d62a8b1bd69f049ea5d20ba41cd9bd62a9445284
Lessie2004MS <97751214+Lessie2004MS@users.noreply.github.com>
2022-01-14 10:39:20 -0600

    Create index.php
    pagina web con  una consulta php

---
+++ index.php
@@ -0,0 +1,225 @@
+<?php
+include'includes\conecta.php';
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Recuperacion</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+    <div class="row">
+      <nav class="navbar navbar-expand-lg navbar-dark"  style="background-color: rgb(147,31,132,0.63)">
+        <div class="container fluid">
+          <div class="d-flex flex-grow-1">
+
+                <a href="#" class="navbar-brand">BellaLess</a>
+
+  <div class="w-100 text-right">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+
+                      <li class="nav-item">
+                               <a href="#" class="nav-link">Nosotros</a>
+                      </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ¿Que ofrecemos?
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><p  href="#"> <svg class="bi" width="15" height="15" fill="light">
+              <use xlink:href="main/icons/bootstrap-icons.svg#layout-text-sidebar-reverse"/>
+            </svg> Labiales</p></li>
+                          <li><hr class="dropdown-divider"></li>
+                               <li><a class="dropdown-item" href="#">Labiales</a></li>
+                                   <li><a class="dropdown-item" href="#">loquidos</a></li>
+                                   <li><a class="dropdown-item" href="#">en barra</a></li>
+                                   <li><a class="dropdown-item" href="#">paletas</a></li>
+                        </ul>
+                      </li>>-->
+                      <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           Contactanos
+                          </a>
+
+                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <li><hr class="dropdown-divider">bellaless@gmail</li>
+
+                          </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                             <svg class="bi" width="15" height="15" fill="light">
+                   <use xlink:href="main/icons/bootstrap-icons.svg#exclamation-circle"/>
+                 </svg>
+                           </a>
+                           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                             <li><a class="dropdown-item" href="#"> Ayuda</a></li>
+                             <li><hr class="dropdown-divider"></li>
+                             <li><a class="dropdown-item" href="#"> Quejas</a></li>
+                           </ul>
+                         </li>
+
+                         <li class="nav-item">
+                                  <a href="login.php" class="nav-link">Inicio de sesion</a>
+                         </li>
+                  </ul>
+          </div>
+      </nav>
+    </div>
+
+    <!-- termina nav -->
+
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#SliderBv5" data-bs-slide-to="0" class="active" style="background-color:rgb(33,47,140)" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#SliderBv5" data-bs-slide-to="1" style="background-color:rgb(33,47,140)" aria-label="Slider 2"></button>
+        <button type="button" data-bs-target="#SliderBv5" data-bs-slide-to="2" style="background-color:rgb(33,47,140)" aria-label="Slider 3"></button>
+    </div>
+
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/logo2.png" class="d-block w-100" alt="primera imagen">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-muted">BellaLess</h5>
+
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/slider2-1.png" class="d-block w-100" alt="segunda imagen">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-muted">BL</h5>
+
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/slider3-1.png" class="d-block w-100" alt="tercera imagen">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-muted">LS</h5>
+
+      </div>
+    </div>
+    </div>
+
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color:rgb(33,47,140)"></span>
+          <span class="visually-hidden">Previous</span>
+    </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true" style="background-color:rgb(33,47,140)"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+
+<hr>
+  <div class="container text-center mt-5">
+<h2>BellaLess es una empresa que ofrece un linea de labiales, un producto no testeado en animales. Tambien es una empresa 100% mexicana
+que bucar el desarrollo laboral de los mexicanos, creando un producto confiable y seguro de usar</h2>
+
+<hr>
+
+</div>
+
+    <div class="container text-center mt-5">
+<h3>MISION</h3>
+<p>BellaLess, tiene como misión ser una empresa competitiva y lider en mercado  de productos de belleza, para asi poder
+satisfacer la necedidad y lujo ue las mujeres desean tener</p>
+</div>
+
+
+    <div class="container text-center mt-5">
+<h3>VISION</h3>
+<p>BellaLess, tiene como visión crecer en el mercado y ser un producto lider a comparacion de otras marcas</p>
+</div>
+
+
+    <div class="container text-center mt-5">
+<h3>VALORES</h3>
+<p></p>
+</div>
+
+    <div class="container text-center mt-5">
+      <h3>¿Que ofrecemos?</h3>
+    <p></p>
+      <div class="row">
+        <div class="container text-center mt-5 col-sms-12 col-md-6 col-lg-6 col-xl-4 py-3">
+          <div class="card shadow">
+      <img src="img/cards1.png" class="card-img-top" alt="servicio1">
+      <div class="card-body">
+          <h4>Plantillas</h4> <hr>
+          <p class="text-muted">Labiales no testeados en aniales</p>
+        </div>
+          </div>
+          </div>
+
+        <div class="container text-center mt-5 col-sms-12 col-md-6 col-lg-6 col-xl-4 py-3">
+          <div class="card shadow">
+      <img src="img/cards2.png" class="card-img-top" alt="servicio2">
+      <div class="card-body">
+          <h4>Materiales de trabajo</h4> <hr>
+          <p class="text-muted">El material de apoyo son fuentes de letras que podras adquirir desde una plataforma llamada Dafont y diversas ideas de como crear
+         pequeños diseños para imagenes y sugerencias para darle un mejor diseño a tus documentos</p>
+        </div>
+          </div>
+            </div>
+      </div>
+    </div>
+
+
+
+
+
+    <div class="container py-3 text-center">
+      <h3 class="text-muted mt-1">BellaLess</h3>
+    </div>
+    <div class="row py-5">
+      <div class="col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
+      <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8 banner">
+           <div class="container py-5">
+             <div class="row text-center">
+               <div class="col">
+                 <svg class="bi  text-light" width="30" height="30" fill="currentColor">
+           <use xlink:href="main/icons/bootstrap-icons.svg#facebook"/>
+           </svg>
+               </div>
+
+               <div class="col">
+                 <svg class="bi  text-light" width="30" height="30" fill="currentColor">
+                 <use xlink:href="main/icons/bootstrap-icons.svg#instagram"/>
+                 </svg>
+               </div>
+
+               <div class="col">
+                 <svg class="bi text-light" width="30" height="30" fill="currentColor">
+                 <use xlink:href="main/icons/bootstrap-icons.svg#whatsapp"/>
+                 </svg>
+               </div>
+
+              <div class="col">
+                 <svg class="bi text-light" width="30" height="30" fill="currentColor">
+               <use xlink:href="main/icons/bootstrap-icons.svg#twitter"/>
+               </svg>
+             </div>
+
+             </div>
+           </div>
+      </div>
+    </div>
+
+
+   <script src="js/bootstrap.min.js" ></script>
+  </body>
+</html>
